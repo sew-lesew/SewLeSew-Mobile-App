5cbb076e56c69e14eb4895392d19a44ff0b74cc8
TitusKI <kidumazie@gmail.com>
2025-02-05 20:26:30 +0300

    Add methods to CampaignRepository for managing business campaigns

--- lib/features/campaign/domain/repository/campaign_repository.dart
+++ lib/features/campaign/domain/repository/campaign_repository.dart
@@ -1,1 +1,12 @@
-abstract class CampaignRepository {}
+import 'package:dartz/dartz.dart';
+import 'package:sewlesew_fund/features/campaign/domain/entities/business_campaign_entity.dart';
+import '../../../../core/resources/failure.dart';
+import '../../../../core/resources/success.dart';
+
+abstract class CampaignRepository {
+  Future<Either<Failure, Success>> createBusinessCampaign(
+      {BusinessCampaignEntity entity});
+  Future<Either<Failure, Success>> getCampaigns();
+  Future<Either<Failure, Success>> getMyCampaigns();
+  Future<Either<Failure, Success>> getCampaignById(String id);
+}
