aa1d6a6816c34bc9d8892eebb813e25c73c10edf
TitusKI <kidumazie@gmail.com>
2025-02-05 20:26:00 +0300

    Refactor CampaignEntity to extend Equatable and include category and deadline fields

--- lib/features/campaign/domain/entities/campaign_entity.dart
+++ lib/features/campaign/domain/entities/campaign_entity.dart
@@ -1,58 +1,40 @@
-import '../../../user_profile/domain/entities/user_entity.dart';
+import 'package:equatable/equatable.dart';
 
-class CampaignEntity {
+import 'campaign_media_entity.dart';
+
+class CampaignEntity extends Equatable {
   final String id;
-  final String userId;
   final String title;
   final String description;
   final double goalAmount;
   final double raisedAmount;
+  final String category;
+  final DateTime deadline;
   final String status;
-  final DateTime createdAt;
-
-  final UserEntity user;
-  final List<String> categories;
   final List<CampaignMediaEntity> campaignMedia;
-  final List<CampaignDocEntity> campaignDocs;
 
-  CampaignEntity({
+  const CampaignEntity({
     required this.id,
-    required this.userId,
     required this.title,
     required this.description,
     required this.goalAmount,
     required this.raisedAmount,
+    required this.category,
+    required this.deadline,
     required this.status,
-    required this.createdAt,
-    required this.user,
-    required this.categories,
     required this.campaignMedia,
-    required this.campaignDocs,
   });
+
+  @override
+  List<Object?> get props => [
+        id,
+        title,
+        description,
+        goalAmount,
+        raisedAmount,
+        category,
+        deadline,
+        status,
+        campaignMedia
+      ];
 }
-
-class CampaignMediaEntity {
-  final String id;
-  final String campaignId;
-  final String url;
-
-  CampaignMediaEntity({
-    required this.id,
-    required this.campaignId,
-    required this.url,
-  });
-}
-
-class CampaignDocEntity {
-  final String id;
-  final String campaignId;
-  final String url;
-
-  CampaignDocEntity({
-    required this.id,
-    required this.campaignId,
-    required this.url,
-  });
-}
-
-enum CampaignStatus { PENDING, ACTIVE, COMPLETED, REJECTED, REMOVED }
