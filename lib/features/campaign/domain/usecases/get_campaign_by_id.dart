d27bda1240720857041762a7078cadf970e4660f
TitusKI <kidumazie@gmail.com>
2025-02-05 20:26:47 +0300

    Add GetCampaignByIdUsecase for retrieving campaign details by ID

---
+++ lib/features/campaign/domain/usecases/get_campaign_by_id.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:sewlesew_fund/core/resources/success_failure.dart';
+import 'package:sewlesew_fund/core/usecase/usecase.dart';
+import 'package:sewlesew_fund/features/campaign/domain/repository/campaign_repository.dart';
+import '../../../../injection_container.dart';
+
+class GetCampaignByIdUsecase extends Usecase<Either<Failure, Success>, String> {
+  @override
+  Future<Either<Failure, Success>> call({String? params}) async {
+    return await sl<CampaignRepository>().getCampaignById(params!);
+  }
+}
