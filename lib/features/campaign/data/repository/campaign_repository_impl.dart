e1956611c150e9284bfc9c53c45a10b9e67cccba
TitusKI <kidumazie@gmail.com>
2025-02-05 20:25:03 +0300

    Implement CampaignRepository with CRUD operations for business campaigns

---
+++ lib/features/campaign/data/repository/campaign_repository_impl.dart
@@ -0,0 +1,117 @@
+import 'package:dartz/dartz.dart';
+import 'package:sewlesew_fund/core/resources/failure.dart';
+import 'package:sewlesew_fund/core/resources/success.dart';
+import 'package:sewlesew_fund/features/campaign/data/mapper/business_campaign_mapper.dart';
+import 'package:sewlesew_fund/features/campaign/data/model/business_campaign_model.dart';
+import 'package:sewlesew_fund/features/campaign/data/model/campaign_detail_model.dart';
+import 'package:sewlesew_fund/features/campaign/data/model/campaign_model.dart';
+import 'package:sewlesew_fund/features/campaign/domain/entities/business_campaign_entity.dart';
+import 'package:sewlesew_fund/features/campaign/domain/entities/campaign_detail_entity.dart';
+import 'package:sewlesew_fund/features/campaign/domain/repository/campaign_repository.dart';
+
+import '../../../../injection_container.dart';
+import '../../domain/entities/campaign_entity.dart';
+import '../mapper/campaign_detail_mapper.dart';
+import '../mapper/campaign_mapper.dart';
+import '../services/remote/campaign_services.dart';
+
+class CampaignRepositoryImpl implements CampaignRepository {
+  @override
+  Future<Either<Failure, Success>> createBusinessCampaign(
+      {BusinessCampaignEntity? entity}) async {
+    try {
+      final BusinessCampaignModel businessCampaignModel =
+          BusinessCampaignMapper.fromEntity(entity!);
+      final campaignFormData =
+          BusinessCampaignMapper.toFormData(businessCampaignModel);
+      final response =
+          await sl<CampaignServices>().createBusinessCampaign(campaignFormData);
+
+      if (response.statusCode == 200) {
+        return Right(Success(message: response.data["message"])); // Success
+      } else {
+        return Left(ServerFailure(
+            response.data["message"] ?? "Campaign Creation Failed"));
+      }
+    } catch (e) {
+      return Left(UnknownFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, Success>> getCampaignById(String id) async {
+    try {
+      final response = await sl<CampaignServices>().getCampaignById(id);
+      final data = response.data['data'];
+      final CampaignDetailModel campaignDetailModel =
+          CampaignDetailModel.fromJson(data);
+      final CampaignDetailEntity campaignDetailEntity =
+          CampaignDetailMapper.toEntity(campaignDetailModel);
+      if (response.statusCode == 200) {
+        return Right(Success(
+            message: response.data['message'],
+            campaignDetailEntity: campaignDetailEntity));
+      } else {
+        return Left(
+            ServerFailure(response.data["message"] ?? "Fetch Campaign Failed"));
+      }
+    } catch (e) {
+      return Left(UnknownFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, Success>> getCampaigns() async {
+    try {
+      final response = await sl<CampaignServices>().getCampaigns();
+      print(response);
+      final data = response.data['data'];
+      print("all campaigns: are $data");
+      final List<CampaignModel> campaignModels = data
+          .map<CampaignModel>((campaign) => CampaignModel.fromJson(campaign))
+          .toList();
+
+      print("campaign models are $campaignModels");
+
+      final List<CampaignEntity> campaignEntities = campaignModels
+          .map<CampaignEntity>((campaign) => CampaignMapper.toEntity(campaign))
+          .toList();
+      print("campaign entities are $campaignEntities");
+      if (response.statusCode == 200) {
+        return Right(Success(
+            message: response.data['message'], campaigns: campaignEntities));
+      } else {
+        return Left(ServerFailure(
+            response.data["message"] ?? "Fetch Campaigns Failed"));
+      }
+    } catch (e) {
+      return Left(UnknownFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, Success>> getMyCampaigns() async {
+    try {
+      final response = await sl<CampaignServices>().getMyCampaign();
+      print(response);
+      final data = response.data['message'];
+      print(data);
+      final List<CampaignModel> myCampaignModels = data
+          .map<CampaignModel>((campaign) => CampaignModel.fromJson(campaign))
+          .toList();
+      final List<CampaignEntity> myCampaigns = myCampaignModels
+          .map<CampaignEntity>((campaign) => CampaignMapper.toEntity(campaign))
+          .toList();
+      print("my campaigns : $myCampaigns");
+      if (response.statusCode == 200) {
+        return Right(
+            Success(message: response.data['message'], campaigns: myCampaigns));
+      } else {
+        return Left(ServerFailure(
+            response.data["message"] ?? "Fetch My Campaigns Failed"));
+      }
+    } catch (e) {
+      return Left(UnknownFailure(e.toString()));
+    }
+  }
+}
