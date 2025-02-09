e06e9584c508375f95bcff212a9f3e084f16b9ac
Tadele21 <tade23dream@gmail.com>
2025-02-09 22:21:07 +0300

    Update BASE_URL in AppConstant for API endpoint configuration

--- lib/core/constants/constant.dart
+++ lib/core/constants/constant.dart
@@ -1,7 +1,8 @@
 class AppConstant {
   static const String STORAGE_DEVICE_OPEN_FIRST_TIME = 'device_first_time';
   static const String USER_TOKEN = 'user_token';
-  static const String BASE_URL = "http://localhost:3333/api";
+  // static const String BASE_URL = "http://localhost:3333/api";
+  static const String BASE_URL = "http://10.161.170.63:3333/api";
   static const String USER_ID_KEY = 'user_id';
   static const String STORAGE_USER_TOKEN_KEY = 'user_token';
 }
