b1bb0996c29349ea6601b9fee2b250df348795d7
Tadele21 <tade23dream@gmail.com>
2025-02-11 03:25:49 +0300

    Refactor BusinessCampaignMapper to streamline entity to model conversion and enhance file handling for campaign uploads

--- lib/features/campaign/data/mapper/business_campaign_mapper.dart
+++ lib/features/campaign/data/mapper/business_campaign_mapper.dart
@@ -4,39 +4,6 @@ import '../../domain/entities/business_campaign_entity.dart';
 import '../model/business_campaign_model.dart';
 
 class BusinessCampaignMapper {
-  // Convert BusinessCampaignModel to BusinessCampaignEntity
-  static BusinessCampaignEntity toEntity(BusinessCampaignModel model) {
-    return BusinessCampaignEntity(
-      fullName: model.fullName,
-      publicEmail: model.publicEmail,
-      publicPhoneNumber: model.publicPhoneNumber,
-      contactEmail: model.contactEmail,
-      contactPhoneNumber: model.contactPhoneNumber,
-      region: model.region,
-      city: model.city,
-      relativeLocation: model.relativeLocation,
-      title: model.title,
-      goalAmount: model.goalAmount,
-      raisedAmount: model.raisedAmount,
-      description: model.description,
-      deadline: model.deadline,
-      website: model.website,
-      bankName: model.bankName,
-      holderName: model.holderName,
-      accountNumber: model.accountNumber,
-      sector: model.sector,
-      tinNumber: model.tinNumber,
-      licenseNumber: model.licenseNumber,
-      category: model.category,
-      tinCertificate: model.tinCertificate,
-      registrationLicense: model.registrationLicense,
-      supportingDocuments: model.supportingDocuments,
-      logo: model.logo,
-      coverImage: model.coverImage,
-      otherImages: model.otherImages,
-    );
-  }
-
   // Convert BusinessCampaignEntity to BusinessCampaignModel
   static BusinessCampaignModel fromEntity(BusinessCampaignEntity entity) {
     return BusinessCampaignModel(
@@ -53,24 +20,20 @@ class BusinessCampaignMapper {
       description: entity.description,
       deadline: entity.deadline,
       website: entity.website,
-      bankName: entity.bankName,
+      bankName: entity.bankName?.name,
       holderName: entity.holderName,
       accountNumber: entity.accountNumber,
-      sector: entity.sector,
+      sector: entity.sector?.name,
       tinNumber: entity.tinNumber,
       licenseNumber: entity.licenseNumber,
-      category: entity.category,
-      tinCertificate: entity.tinCertificate,
-      registrationLicense: entity.registrationLicense,
-      supportingDocuments: entity.supportingDocuments,
-      logo: entity.logo,
-      coverImage: entity.coverImage,
-      otherImages: entity.otherImages,
+      category: entity.category?.name,
     );
   }
 
-  static Future<FormData> toFormData(BusinessCampaignModel model) async {
+  static Future<FormData> toFormData(
+      BusinessCampaignModel model, BusinessCampaignEntity entity) async {
     return FormData.fromMap({
+// all below till reach the sector are also common for all type
       'fullName': model.fullName,
       'publicEmail': model.publicEmail,
       'publicPhoneNumber': model.publicPhoneNumber,
@@ -82,39 +45,52 @@ class BusinessCampaignMapper {
       'title': model.title,
       'goalAmount': model.goalAmount.toString(),
       'description': model.description,
-      'deadline': model.deadline,
-      'website': model.website,
+      'deadline':
+          DateTime.parse(model.deadline).toIso8601String().split('T').first,
       'bankName': model.bankName,
       'holderName': model.holderName,
       'accountNumber': model.accountNumber,
-      'sector': model.sector,
-      'tinNumber': model.tinNumber,
-      'licenseNumber': model.licenseNumber,
       'category': model.category,
-
       // Single file uploads
-      if (model.tinCertificate != null)
+      if (entity.tinCertificate != null)
         'tinCertificate':
-            await MultipartFile.fromFile(model.tinCertificate!.path),
-      if (model.registrationLicense != null)
+            await MultipartFile.fromFile(entity.tinCertificate!.path),
+      if (entity.registrationLicense != null)
         'registrationLicense':
-            await MultipartFile.fromFile(model.registrationLicense!.path),
-      if (model.logo != null)
-        'logo': await MultipartFile.fromFile(model.logo!.path),
-      if (model.coverImage != null)
-        'coverImage': await MultipartFile.fromFile(model.coverImage!.path),
-
+            await MultipartFile.fromFile(entity.registrationLicense!.path),
+      if (entity.coverImage != null)
+        'coverImage': await MultipartFile.fromFile(entity.coverImage!.path),
+      if (entity.personalDocuments != null)
+        'personalDocuments':
+            await MultipartFile.fromFile(entity.personalDocuments!.path),
       // List of files for supportingDocuments
-      if (model.supportingDocuments != null)
-        'supportingDocuments': await Future.wait(model.supportingDocuments!.map(
+      if (entity.supportingDocuments != null)
+        'supportingDocuments':
+            await Future.wait(entity.supportingDocuments!.map(
           (file) async => await MultipartFile.fromFile(file.path),
         )),
 
       // List of files for otherImages
-      if (model.otherImages != null)
-        'otherImages': await Future.wait(model.otherImages!.map(
+      if (entity.otherImages != null)
+        'otherImages': await Future.wait(entity.otherImages!.map(
           (file) async => await MultipartFile.fromFile(file.path),
         )),
+
+// The above all are common to the three types of campaigns
+// Here it starts to differentiate between the three types of campaigns
+// the below three are only for business campaign
+// That means they will be send to the api as null for the other two types of campaigns
+      'sector': model.sector,
+      'tinNumber': model.tinNumber,
+      'licenseNumber': model.licenseNumber,
+
+      // logo for only both organizatonal and business
+      // That means they will be send to the api as null for the  personal campaigns
+      if (entity.logo != null)
+        'logo': await MultipartFile.fromFile(entity.logo!.path),
+
+// The below are only for personal campaign
+// That means they will be send to the api as null for the other two types of campaigns
     });
   }
 }
