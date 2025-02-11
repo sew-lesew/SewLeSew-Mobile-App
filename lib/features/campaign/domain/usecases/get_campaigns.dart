48b93f68cfa3a201f8d196e52f3f655db51c5c1d
TitusKI <kidumazie@gmail.com>
2025-02-11 04:42:38 +0300

    Refactor GetCampaignsUsecase to accept CampaignParameter for improved flexibility in fetching campaigns

--- lib/features/campaign/domain/usecases/get_campaigns.dart
+++ lib/features/campaign/domain/usecases/get_campaigns.dart
@@ -4,9 +4,19 @@ import 'package:sewlesew_fund/core/usecase/usecase.dart';
 import 'package:sewlesew_fund/features/campaign/domain/repository/campaign_repository.dart';
 import '../../../../injection_container.dart';
 
-class GetCampaignsUsecase extends Usecase<Either<Failure, Success>, void> {
+class GetCampaignsUsecase
+    extends Usecase<Either<Failure, Success>, CampaignParameter> {
   @override
-  Future<Either<Failure, Success>> call({void params}) async {
-    return await sl<CampaignRepository>().getCampaigns();
+  Future<Either<Failure, Success>> call({CampaignParameter? params}) async {
+    return await sl<CampaignRepository>()
+        .getCampaigns(category: params!.category, name: params.name);
   }
+}
+
+// parameter for get campaigns
+class CampaignParameter {
+  final String? category;
+  final String? name;
+
+  CampaignParameter({this.category, this.name});
 }
