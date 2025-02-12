6283f53ffaa0c9c19480358cb276416809181dfd
TitusKI <kidumazie@gmail.com>
2025-02-12 10:37:03 +0300

    Update password visibility icons in TogglePasswordBloc

--- lib/features/auth/presentation/bloc/toogle_password/toggle_password_bloc.dart
+++ lib/features/auth/presentation/bloc/toogle_password/toggle_password_bloc.dart
@@ -1,6 +1,5 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 part 'toggle_password_event.dart';
@@ -13,8 +12,8 @@ class TogglePasswordBloc
       emit(state.copyWith(
           obscurePassword: !state.obscurePassword,
           iconPassword: state.obscurePassword
-              ? CupertinoIcons.eye_slash_fill
-              : Icons.remove_red_eye_rounded));
+              ? Icons.visibility_off_outlined
+              : Icons.visibility_outlined));
     });
   }
 }
