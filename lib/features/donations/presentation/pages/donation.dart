ba4847e668fc7427fa3656d6a4dd525b668e0e0e
TitusKI <kidumazie@gmail.com>
2025-02-15 00:16:41 +0300

    Refactor Donation page to use Bloc for state management and display user donations

--- lib/features/donations/presentation/pages/donation.dart
+++ lib/features/donations/presentation/pages/donation.dart
@@ -1,32 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sewlesew_fund/core/resources/generic_state.dart';
 import 'package:sewlesew_fund/features/donations/presentation/widgets/donation_widgets.dart';
 
 import '../../../../config/theme/colors.dart';
+import '../../../auth/presentation/widgets/flutter_toast.dart';
+import '../../../campaign/presentation/bloc/campaign_cubit.dart';
+import '../../../campaign/presentation/pages/filtered_campaign.dart';
+import '../../domain/entities/donation_entity.dart';
+import '../bloc/donation_cubit.dart';
+import 'donation_detail.dart';
 
-class Donation extends StatelessWidget {
+class Donation extends StatefulWidget {
   const Donation({super.key});
+
+  @override
+  State<Donation> createState() => _DonationState();
+}
+
+class _DonationState extends State<Donation> {
+  @override
+  void initState() {
+    super.initState();
+    // context.read<CampaignCubit>().getCampaigns();
+    // context.read<DonationCubit>().getDonationByCampaign();
+    context.read<DonationCubit>().getDonationByUser();
+  }
 
   // Helper method to build a previous donation tile
   Widget _buildPreviousDonationTile() {
-    return Card(
-      // color: AppColors.cardColor,
-      margin: const EdgeInsets.symmetric(vertical: 8),
-      child: ListTile(
-        leading: CircleAvatar(
-          backgroundColor: AppColors.accentColor.withOpacity(0.5),
-          child: const Icon(Icons.campaign, color: AppColors.accentColor),
-        ),
-        title: const Text(
-          "Education for All Campaign",
-          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-        ),
-        subtitle: const Text(
-          "Donated: 100birr\nDate: Nov 10, 2024",
-          style: TextStyle(fontSize: 14, color: Colors.grey),
-        ),
-        trailing: const Icon(Icons.arrow_forward_ios,
-            size: 16, color: AppColors.accentColor),
-      ),
+    return BlocBuilder<DonationCubit, GenericState>(
+      builder: (context, state) {
+        if (state.isLoading ?? false) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        // if (state.failure != null) {
+        //   return toastInfo(msg: state.failure);
+        // }
+
+        final data = state.data as List<DonationEntity>;
+        if (data.isEmpty) {
+          return Text("No donations available");
+        }
+
+        return ListView.builder(
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: data.length,
+          itemBuilder: (context, index) {
+            final donation = data[index];
+            return Card(
+              margin: const EdgeInsets.symmetric(vertical: 8),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              DonationDetail(donation: donation)));
+                },
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: AppColors.accentColor.withAlpha(128),
+                    child: const Icon(Icons.campaign,
+                        color: AppColors.accentColor),
+                  ),
+                  title: Text(
+                    "${donation.donorFirstName} ${donation.donorLastName}",
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.w600),
+                  ),
+                  subtitle: Text(
+                    "Amount: ${donation.amount} birr",
+                    style: const TextStyle(fontSize: 14, color: Colors.grey),
+                  ),
+                  trailing: const Icon(Icons.arrow_forward_ios,
+                      size: 16, color: AppColors.accentColor),
+                ),
+              ),
+            );
+          },
+        );
+      },
     );
   }
 
@@ -71,20 +129,30 @@ class Donation extends StatelessWidget {
 
   // Helper method to build a cause card
   Widget _buildCauseCard(String title, IconData icon, BuildContext context) {
-    return Column(
-      children: [
-        CircleAvatar(
-          radius: 30,
-          backgroundColor: AppColors.cardColor.withOpacity(0.5),
-          child: Icon(icon, color: AppColors.accentColor, size: 28),
-        ),
-        const SizedBox(height: 8),
-        Text(
-          title,
-          style: Theme.of(context).textTheme.bodySmall,
-          textAlign: TextAlign.center,
-        ),
-      ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => CategoryCampaignScreen(),
+          ),
+        );
+      },
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 30,
+            backgroundColor: AppColors.cardColor.withOpacity(0.5),
+            child: Icon(icon, color: AppColors.accentColor, size: 28),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            title,
+            style: Theme.of(context).textTheme.bodySmall,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
     );
   }
 
@@ -118,8 +186,8 @@ class Donation extends StatelessWidget {
                 shrinkWrap: true,
                 physics: const NeverScrollableScrollPhysics(),
                 crossAxisCount: 3,
-                crossAxisSpacing: 20,
-                mainAxisSpacing: 20,
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
                 children: [
                   _buildCauseCard("Education", Icons.school, context),
                   _buildCauseCard("Healthcare", Icons.local_hospital, context),
@@ -134,12 +202,25 @@ class Donation extends StatelessWidget {
               // My Donations Section
               lineWithText(text: " My Donations", context: context),
               const SizedBox(height: 10),
-              ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: 3, // Example previous donations
-                itemBuilder: (context, index) => _buildPreviousDonationTile(),
-              ),
+              _buildPreviousDonationTile(),
+              // FutureBuilder(
+              //   future: context.read<DonationCubit>().getDonationByUser(),
+              //   builder: (context, snapshot) {
+              //     if (snapshot.connectionState == ConnectionState.waiting) {
+              //       return Center(child: CircularProgressIndicator());
+              //     } else if (snapshot.hasError) {
+              //       return Center(child: Text('Error: ${snapshot.error}'));
+              //     } else {
+              //       return ListView.builder(
+              //         shrinkWrap: true,
+              //         physics: const NeverScrollableScrollPhysics(),
+              //         itemCount: 3, // Example previous donations
+              //         itemBuilder: (context, index) =>
+              //             _buildPreviousDonationTile(),
+              //       );
+              //     }
+              //   },
+              // ),
               const SizedBox(height: 20),
 
               // Final Note and Call to Action
