ac4f0b321078a564980fc31817b85d1e9490e1de
TitusKI <kidumazie@gmail.com>
2025-02-14 09:32:09 +0300

    Update BASE_URL constant for API endpoint configuration

--- lib/core/constants/constant.dart
+++ lib/core/constants/constant.dart
@@ -1,8 +1,17 @@
+// ignore_for_file: constant_identifier_names
+
 class AppConstant {
   static const String STORAGE_DEVICE_OPEN_FIRST_TIME = 'device_first_time';
   static const String USER_TOKEN = 'user_token';
   // static const String BASE_URL = "http://localhost:3333/api";
-  static const String BASE_URL = "http://10.161.170.63:3333/api";
+  // bit
+  // static const String BASE_URL = "http://10.161.170.63:3333/api";
+  // Tade
+  // static const String BASE_URL = "http://192.168.112.2:3333/api";
+  // Enola
+  // static const String BASE_URL = "http://192.168.43.184:3333/api";
+  // Dagi
+  static const String BASE_URL = "http://192.168.8.56:3333/api";
   static const String USER_ID_KEY = 'user_id';
   static const String STORAGE_USER_TOKEN_KEY = 'user_token';
 }
