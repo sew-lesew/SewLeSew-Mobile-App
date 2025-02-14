2acf68476b476e70a28237ecf2d4d5ff2c554be0
TitusKI <kidumazie@gmail.com>
2025-02-14 09:44:32 +0300

    Refactor GetMyCampaignsUsecase to accept campaignStatus as a parameter for improved campaign filtering

--- lib/features/campaign/domain/usecases/get_my_campaigns.dart
+++ lib/features/campaign/domain/usecases/get_my_campaigns.dart
@@ -4,9 +4,10 @@ import 'package:sewlesew_fund/core/usecase/usecase.dart';
 import 'package:sewlesew_fund/features/campaign/domain/repository/campaign_repository.dart';
 import '../../../../injection_container.dart';
 
-class GetMyCampaignsUsecase extends Usecase<Either<Failure, Success>, void> {
+class GetMyCampaignsUsecase extends Usecase<Either<Failure, Success>, String?> {
   @override
-  Future<Either<Failure, Success>> call({void params}) async {
-    return await sl<CampaignRepository>().getMyCampaigns();
+  Future<Either<Failure, Success>> call({String? params}) async {
+    return await sl<CampaignRepository>()
+        .getMyCampaigns(campaignStatus: params);
   }
 }
