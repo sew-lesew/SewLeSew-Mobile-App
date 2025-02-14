5ec17b61fbd8892b88ae7a0c16b9a128b48af9cd
TitusKI <kidumazie@gmail.com>
2025-02-14 09:33:21 +0300

    Refactor VerificationEvent to remove Equatable implementation and simplify event structure

--- lib/features/auth/presentation/bloc/verification/verification_event.dart
+++ lib/features/auth/presentation/bloc/verification/verification_event.dart
@@ -1,17 +1,12 @@
 part of 'verification_bloc.dart';
 
-sealed class VerificationEvent extends Equatable {
+sealed class VerificationEvent {
   const VerificationEvent();
-
-  @override
-  List<Object> get props => [];
 }
 
 class CodeChanged extends VerificationEvent {
   final String code;
   const CodeChanged(this.code);
-  @override
-  List<Object> get props => [code];
 }
 
 class SubmitCode extends VerificationEvent {
@@ -19,16 +14,15 @@ class SubmitCode extends VerificationEvent {
   final String? email;
   final String? phoneNumber;
   const SubmitCode({this.code, this.email, this.phoneNumber});
-  @override
-  List<Object> get props => [code!, email!, phoneNumber!];
+  // @override
+  // List<Object> get props => [code!, email, phoneNumber!];
 }
 
 class ResendCode extends VerificationEvent {
   final String? email;
   final String? phoneNumber;
   const ResendCode({this.email, this.phoneNumber});
-  @override
-  List<Object> get props => [email!, phoneNumber!];
+  // List<Object> get props => [email!, phoneNumber!];
 }
 
 class ResetCode extends VerificationEvent {}
