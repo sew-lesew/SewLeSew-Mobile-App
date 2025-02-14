310bd07f487bcc1146973db21da688e9d499ddfc
TitusKI <kidumazie@gmail.com>
2025-02-15 00:20:08 +0300

    Refactor Profile page to integrate dynamic profile image and user details from ProfileCubit

--- lib/features/user_profile/presentation/pages/profile.dart
+++ lib/features/user_profile/presentation/pages/profile.dart
@@ -1,8 +1,13 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sewlesew_fund/features/donations/presentation/widgets/donation_widgets.dart';
 
 import '../../../../config/routes/names.dart';
 import '../../../../config/theme/colors.dart';
+import '../../../donations/presentation/pages/my_donation.dart';
+import '../../domain/entities/profile_entity.dart';
+import '../bloc/profile_cubit.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
@@ -11,15 +16,17 @@ class Profile extends StatelessWidget {
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => ExpandedPage(title: pageTitle),
+        builder: (context) => MyDonationPage(),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    // Access the ProfileEntity from your state management (example using Provider)
+    final ProfileEntity profileEntity = context.read<ProfileCubit>().state.data;
+
     return Scaffold(
-      // backgroundColor: AppColors.primaryBackground,
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
@@ -35,27 +42,36 @@ class Profile extends StatelessWidget {
                   padding: const EdgeInsets.all(16.0),
                   child: Row(
                     children: [
-                      // Profile Image Placeholder
+                      // Dynamic Profile Image/Initial
                       CircleAvatar(
                         radius: 40,
                         backgroundColor: AppColors.primaryBackground,
-                        child: const Text(
-                          'K',
-                          style: TextStyle(
-                            fontSize: 32,
-                            fontWeight: FontWeight.bold,
-                            color: AppColors.accentColor,
-                          ),
-                        ),
+                        backgroundImage: profileEntity.profilePictueUrl != null
+                            ? CachedNetworkImageProvider(
+                                profileEntity.profilePictueUrl!,
+                              )
+                            : null,
+                        child: profileEntity.profilePictueUrl == null
+                            ? Text(
+                                profileEntity.firstName!.isNotEmpty
+                                    ? profileEntity.firstName![0].toUpperCase()
+                                    : '',
+                                style: const TextStyle(
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.bold,
+                                  color: AppColors.accentColor,
+                                ),
+                              )
+                            : null,
                       ),
                       const SizedBox(width: 16),
                       // Profile Details
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          const Text(
-                            'Kidan',
-                            style: TextStyle(
+                          Text(
+                            profileEntity.firstName!,
+                            style: const TextStyle(
                               fontSize: 18,
                               fontWeight: FontWeight.bold,
                             ),
@@ -96,9 +112,9 @@ class Profile extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   _buildImpactTile(
-                      'Lives Supported', '0', Icons.people_outline),
-                  _buildImpactTile(
-                      'Total Donation', '0', Icons.monetization_on_outlined),
+                      'Lives Supported', '3', Icons.people_outline),
+                  _buildImpactTile('Total Donation', '155555',
+                      Icons.monetization_on_outlined),
                 ],
               ),
               const SizedBox(height: 20),
@@ -230,162 +246,3 @@ class ExpandedPage extends StatelessWidget {
     );
   }
 }
-
-// class Profile extends StatelessWidget {
-//   const Profile({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SingleChildScrollView(
-//       child: Column(
-//         children: [
-//           Stack(
-//             children: [
-//               Container(
-//                 height: 200,
-//                 decoration: const BoxDecoration(
-//                   color: AppColors.accentColor,
-//                   borderRadius: BorderRadius.only(
-//                     bottomLeft: Radius.circular(40),
-//                     bottomRight: Radius.circular(40),
-//                   ),
-//                 ),
-//               ),
-//               const Column(
-//                 children: [
-//                   SizedBox(height: 40),
-//                   Center(
-//                     child: CircleAvatar(
-//                       radius: 60,
-//                       backgroundColor: Colors.white,
-//                       child: CircleAvatar(
-//                         radius: 55,
-//                         backgroundImage:
-//                             AssetImage('assets/welcome/welcome1.png'),
-//                       ),
-//                     ),
-//                   ),
-//                   SizedBox(height: 10),
-//                   Text(
-//                     "Kidan Mazie",
-//                     style: TextStyle(
-//                       fontSize: 22,
-//                       fontWeight: FontWeight.bold,
-//                       color: Colors.white,
-//                     ),
-//                   ),
-//                   Text(
-//                     "Software Developer",
-//                     style: TextStyle(
-//                       fontSize: 16,
-//                       color: Colors.white70,
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             ],
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.all(16.0),
-//             child: Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 const SizedBox(height: 20),
-//                 const SectionHeader(title: "Personal Information"),
-//                 const SizedBox(height: 10),
-//                 _buildDetailTile(
-//                   icon: Icons.person,
-//                   title: "Full Name",
-//                   value: "Kidan Mazie",
-//                 ),
-//                 _buildDetailTile(
-//                   icon: Icons.phone,
-//                   title: "Mobile Number",
-//                   value: "+251 921 889 274",
-//                 ),
-//                 _buildDetailTile(
-//                   icon: Icons.email,
-//                   title: "Email Address",
-//                   value: "kidumazie@gmail.com",
-//                 ),
-//                 const SizedBox(height: 30),
-//                 const SectionHeader(title: "Other Details"),
-//                 const SizedBox(height: 10),
-//                 _buildDetailTile(
-//                   icon: Icons.location_on,
-//                   title: "Location",
-//                   value: "Addis Ababa, Ethiopia",
-//                 ),
-//                 _buildDetailTile(
-//                   icon: Icons.cake,
-//                   title: "Date of Birth",
-//                   value: "5th July 1994",
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-
-//   Widget _buildDetailTile({
-//     required IconData icon,
-//     required String title,
-//     required String value,
-//   }) {
-//     return Padding(
-//       padding: const EdgeInsets.symmetric(vertical: 10),
-//       child: Row(
-//         children: [
-//           CircleAvatar(
-//             radius: 20,
-//             backgroundColor: AppColors.accentColor.withOpacity(0.1),
-//             child: Icon(icon, color: AppColors.accentColor),
-//           ),
-//           const SizedBox(width: 15),
-//           Expanded(
-//             child: Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 Text(
-//                   title,
-//                   style: const TextStyle(
-//                     fontSize: 14,
-//                     color: Colors.grey,
-//                   ),
-//                 ),
-//                 const SizedBox(height: 5),
-//                 Text(
-//                   value,
-//                   style: const TextStyle(
-//                     fontSize: 16,
-//                     fontWeight: FontWeight.w500,
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// class SectionHeader extends StatelessWidget {
-//   final String title;
-
-//   const SectionHeader({required this.title, super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Text(
-//       title,
-//       style: const TextStyle(
-//         fontSize: 18,
-//         fontWeight: FontWeight.bold,
-//         color: AppColors.accentColor,
-//       ),
-//     );
-//   }
-// }
