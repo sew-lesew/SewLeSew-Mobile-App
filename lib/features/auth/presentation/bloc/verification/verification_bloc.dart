181f9e49d66131a33d6d88f9c15984761e0d5dc7
TitusKI <kidumazie@gmail.com>
2025-02-14 09:33:12 +0300

    Refactor VerificationBloc to simplify code flow and remove redundant error handling

--- lib/features/auth/presentation/bloc/verification/verification_bloc.dart
+++ lib/features/auth/presentation/bloc/verification/verification_bloc.dart
@@ -19,51 +19,19 @@ class VerificationBloc extends Bloc<VerificationEvent, VerificationState> {
         emit(VerificationCodeInvalid(event.code));
       }
     });
-    on<SubmitCode>((event, emit) async {
-      emit(VerificationLoading());
-      try {
-        await sl<VerifyAccountUsecase>().call(
-          params: VerifyAccountParams(
-            email: event.email,
-            code: event.code,
-            phoneNumber: EthiopianPhoneValidator.normalize(event.phoneNumber),
-          ),
-        );
 
-        emit(VerificationSuccess());
-      } catch (e) {
-        if (e.toString().contains("401")) {
-          try {
-            await sl<RefreshUsecase>().call();
-            await sl<VerifyAccountUsecase>().call(
-              params: VerifyAccountParams(
-                email: event.email,
-                code: event.code,
-                phoneNumber:
-                    EthiopianPhoneValidator.normalize(event.phoneNumber),
-              ),
-            );
-            emit(VerificationSuccess());
-          } catch (refreshError) {
-            emit(const VerificationFailure(
-                "Token refresh failed. Please try again."));
-          }
-        } else {
-          emit(VerificationFailure(e.toString()));
-        }
-      }
+    on<SubmitCode>((event, emit) async {
+      await sl<VerifyAccountUsecase>().call(
+        params: VerifyAccountParams(
+          email: event.email,
+          code: event.code,
+          phoneNumber: event.phoneNumber,
+        ),
+      );
+
+      emit(VerificationSuccess());
     });
 
-    // on<SubmitResetCode>((event, emit) async {
-    //   emit(VerificationLoading());
-    //   try {
-    //     await _repository.forgotPassword(event.email!);
-    //     emit(VerificationSuccess());
-    //     print("Reset this ${event.email} ");
-    //   } catch (e) {
-    //     emit(VerificationFailure(e.toString()));
-    //   }
-    // });
     on<ResendCode>((event, emit) async {
       emit(VerificationLoading());
       try {
@@ -72,12 +40,12 @@ class VerificationBloc extends Bloc<VerificationEvent, VerificationState> {
                 email: event.email,
                 phoneNumber:
                     EthiopianPhoneValidator.normalize(event.phoneNumber)));
-        // await sl<AuthRepository>().resendCode(event.email);
         emit(VerificationCodeResent());
       } catch (e) {
         emit(VerificationFailure(e.toString()));
       }
     });
+
     on<ResetCode>((event, emit) {
       emit(VerificationInitial());
     });
