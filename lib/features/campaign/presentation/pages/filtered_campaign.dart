b08773bdf07da91aae8a87fa3304399c06aca25b
TitusKI <kidumazie@gmail.com>
2025-02-14 09:49:09 +0300

    Refactor CategoryCampaignScreen to handle nullable category and improve campaign data handling

--- lib/features/campaign/presentation/pages/filtered_campaign.dart
+++ lib/features/campaign/presentation/pages/filtered_campaign.dart
@@ -1,17 +1,23 @@
 import 'dart:async';
+import 'package:dartz/dartz.dart' as dartz;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sewlesew_fund/core/resources/generic_state.dart';
+import 'package:sewlesew_fund/core/resources/success_failure.dart';
 
 import '../../../../core/constants/enum_campaign.dart';
 import '../../../../core/util/enum_string.dart';
+import '../../../donations/presentation/pages/payment.dart';
+import '../../../explore/presentation/widgets/calculate_days_left.dart';
+import '../../../explore/presentation/widgets/campaign_card_shimmer.dart';
+import '../../domain/entities/campaign_entity.dart';
 import '../bloc/campaign_cubit.dart';
 import '../widgets/campaign_card.dart';
 
 class CategoryCampaignScreen extends StatefulWidget {
-  final Category category;
+  final Category? category;
 
-  const CategoryCampaignScreen({super.key, required this.category});
+  const CategoryCampaignScreen({super.key, this.category});
 
   @override
   _CategoryCampaignScreenState createState() => _CategoryCampaignScreenState();
@@ -52,7 +58,9 @@ class _CategoryCampaignScreenState extends State<CategoryCampaignScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(enumToDisplayString(widget.category)),
+        title: Text(widget.category != null
+            ? enumToDisplayString(widget.category!)
+            : "All Campaigns"),
       ),
       body: Column(
         children: [
@@ -67,31 +75,48 @@ class _CategoryCampaignScreenState extends State<CategoryCampaignScreen> {
             ),
           ),
           Expanded(
-            child: BlocBuilder<CampaignCubit, GenericState>(
+            child: BlocBuilder<CampaignCubit,
+                GenericState<dartz.Either<Failure, Success>>>(
               builder: (context, state) {
                 if (state.isLoading!) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (state.failure != null) {
-                  return Center(child: Text("Error: ${state.failure}"));
-                } else if (state.data.isEmpty) {
-                  return const Center(child: Text("No campaigns found"));
+                  return campaignCardSkeleton(context);
                 }
+                // if (state.failure != null) {
+                //   return Center(
+                //       child: Text("Error: ${dartz.Left(state.failure!)}"));
+                // }
+                final data = state.data;
+                if (data == null || data.isLeft()) {
+                  return const Center(child: Text("No campaigns found."));
+                }
+                final campaignData = (data as dartz.Right).value;
+                final List<CampaignEntity> campaigns =
+                    campaignData.campaigns ?? [];
+                print(campaigns);
                 return ListView.builder(
-                  itemCount: state.data.length,
+                  itemCount: campaigns.length,
                   itemBuilder: (context, index) {
-                    final campaign = state.data[index];
+                    final CampaignEntity campaign = campaigns[index];
                     return CampaignCard(
-                      goalAmount: campaign.goalAmount,
-                      raisedAmount: campaign.raisedAmount,
+                      goalAmount: campaign.goalAmount.toString(),
+                      raisedAmount: campaign.raisedAmount.toString(),
                       campaignId: campaign.id,
                       status: campaign.status,
                       title: campaign.title,
                       description: campaign.description,
-                      dayLeft: campaign.daysLeft,
-                      progress: campaign.progress,
-                      donors: campaign.donors,
-                      imageUrl: campaign.imageUrl,
-                      onDonateNowPressed: () {},
+                      dayLeft: "${calculateDaysLeft(campaign.deadline)} days",
+                      progress: double.parse(
+                          (campaign.raisedAmount / campaign.goalAmount * 100)
+                              .toStringAsFixed(2)),
+                      donors: campaign.donationCount,
+                      imageUrl: campaign.campaignMedia[0].url,
+                      onDonateNowPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (_) =>
+                                    DonatePayment(campaignId: campaign.id)));
+                      },
                     );
                   },
                 );
